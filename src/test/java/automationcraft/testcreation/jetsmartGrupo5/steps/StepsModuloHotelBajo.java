a5abf5206b7fbf4658de59c121c3ef649baf999e
Kevin Pacheco <Kevin.Pacheco@tsoftglobal.com>
2021-05-10 23:10:41 -0300

    Buenas, agrego uno

---
+++ src/test/java/automationcraft/testcreation/jetsmartGrupo5/steps/StepsModuloHotelBajo.java
@@ -0,0 +1,38 @@
+package automationcraft.testcreation.jetsmartGrupo5.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepsModuloHotelBajo {
+
+    @Given("soy el usuario e ingreso a la pagina {string}")
+    public void soy_un_usuario_que_ingreso_a_la_web(String string){
+
+    }
+
+    @When("Ingreso al formulario Hoteles")
+    public void Ingreso_al_formulario_Hoteles(){
+
+    }
+
+    @And("selecciono {string} en el campo Origen")
+    public void selecciono_en_el_campo_Origen(String string){
+
+    }
+
+    @And("presiono el boton Buscar")
+    public void presiono_el_boton_Buscar(){
+
+    }
+
+    @Then("No se encuentra paquetes de alojamiento en Antartica")
+    public void No_se_encuentra_paquetes_de_alojamiento_en_Antartica(){
+
+    }
+
+
+
+
+}
