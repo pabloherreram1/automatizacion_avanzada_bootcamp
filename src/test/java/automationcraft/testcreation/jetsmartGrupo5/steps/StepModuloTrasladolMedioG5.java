3ad409410fa249310e0bc30a7c27989058901c34
Pamela Peralta <Pamela.Peralta@tsoftglobal.com>
2021-05-10 23:32:18 -0300

    Eran steps jaja, saludos. Se agrega step traslados medios e.e

---
+++ src/test/java/automationcraft/testcreation/jetsmartGrupo5/steps/StepModuloTrasladolMedioG5.java
@@ -0,0 +1,55 @@
+package automationcraft.testcreation.jetsmartGrupo5.steps;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepModuloTrasladolMedioG5 {
+    @Given("soy el usuario e ingreso a la pagina Jetsmart ")
+    public void soy_el_usuario_e_ingreso_a_la_web(String string){
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("ingreso al formulario traslados")
+    public void ingreso_al_formulario_de_traslados(){
+
+    }
+    @And("Seleciono radio boton ida y vuelta")
+    public void Seleciono_radio_boton_ida_y_vuelta(String string){
+
+    }
+    @And("Seleciono {String} en el campo Origen")
+    public void Seleciono_en_el_campo_Origen(String string){
+
+    }
+    @When("Seleciono {String} en el campo Destino")
+    public void Seleciono_en_el_campo_Destino(String string){
+
+    }
+
+    @And("selecciono una fecha de ida en 30 dias")
+    public void Selecciono_una_fecha_de_ida_en_30_dias(String string){
+
+    }
+    @And("Selecciono el horario de las 9 am")
+    public void Selecciono_horario_9_am(String string){
+
+    }
+    @And("pSeleciono una fecha de vuelta en 31 dias de la fecha actual")
+    public void Seleciono_fecha_vuelta_en_31_dias_de_la_fecha_actual(String string){
+
+    }
+    @And("Seleciono el horario de vuelta Actual")
+    public void Selecciono_horario_de_vuelta_Actual(String string){
+
+    }
+    @And("Presiono el boton Buscar")
+    public void Presiono_el_boton_buscar(String string){
+
+        }
+    @Then("Se muestra que no se han encontrado resultados para el traslado")
+    public void Se_muestra_que_no_se_han_encontrado_resultados_para_el_traslado(String string){
+
+    }
+}
